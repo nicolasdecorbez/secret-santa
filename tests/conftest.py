abc830a26b5b59a4dbdac015686177412a458028
decorb_n <decorb_n@etna-alternance.net>
2022-02-28 00:29:06 +0100

    fix(typo): call class instead of modules

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,47 +3,47 @@ import pytest
 from secret_santa import Pair, Person, PairsList, PersonsList
 
 @pytest.fixture
-def valid_person() -> Person:
+def valid_person() -> Person.Person:
     """Return a valid Person()"""
-    return Person(
+    return Person.Person(
         name="person",
         email="person@email.com"
     )
 
 @pytest.fixture
-def valid_pair() -> Pair:
+def valid_pair() -> Pair.Pair:
     """Return a valid Pair()"""
-    return Pair(
-        sender=Person("sender", "sender@email.com"),
-        receiver=Person("receiver", "receiver@email.com")
+    return Pair.Pair(
+        sender=Person.Person("sender", "sender@email.com"),
+        receiver=Person.Person("receiver", "receiver@email.com")
     )
 
 @pytest.fixture
-def valid_person_list() -> PersonsList:
+def valid_person_list() -> PersonsList.PersonsList:
     """Return a valid PersonList()"""
-    persons_list = PersonsList()
+    persons_list = PersonsList.PersonsList()
     persons_list.register(
-        Person("p1", "p1@email.com")
+        Person.Person("p1", "p1@email.com")
     )
     persons_list.register(
-        Person("p2", "p2@email.com")
+        Person.Person("p2", "p2@email.com")
     )
     return persons_list
 
 @pytest.fixture
-def valid_pair_lists() -> PairsList:
+def valid_pair_lists() -> PairsList.PairsList:
     """Return a valid PersonList()"""
-    pairs_list = PairsList()
+    pairs_list = PairsList.PairsList()
     pairs_list.register(
-        Pair(
-            Person("s1", "s1@email.com"),
-            Person("r1", "r1@email.com")
+        Pair.Pair(
+            Person.Person("s1", "s1@email.com"),
+            Person.Person("r1", "r1@email.com")
         )
     )
     pairs_list.register(
-        Pair(
-            Person("s2", "s1@email.com"),
-            Person("r2", "r1@email.com")
+        Pair.Pair(
+            Person.Person("s2", "s1@email.com"),
+            Person.Person("r2", "r1@email.com")
         )
     )
     return pairs_list
